c5adea0119191530e84a05a1d7d89b59bcb1b972
Madi Rysbek <hooizit@gmail.com>
2022-06-05 11:09:17 +0600

    feat: change fetch to axios

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import axios from "axios";
+
 import { SearchContext } from "../App";
 
 import Categories from "../components/Categories";
@@ -11,47 +13,41 @@ import { setCategory } from "../redux/slices/filterSlice";
 
 const Home = () => {
   const dispatch = useDispatch();
-  const {category, sort, order} = useSelector((state) => state.filter);
+  const { category, sort, order } = useSelector((state) => state.filter);
 
-  const {searchValue } = React.useContext(SearchContext);
+  const { searchValue } = React.useContext(SearchContext);
   const [isLoading, setIsLoading] = React.useState(true);
   const [items, setItems] = React.useState([]);
   const [currentPage, setCurrentPage] = React.useState(1);
 
   const onCahngeCategory = (category) => {
     dispatch(setCategory(category));
-  }
+  };
 
   React.useEffect(() => {
     setIsLoading(true);
-    fetch(
-      `https://628e18f4368687f3e7104a3b.mockapi.io/items?page=${currentPage}&limit=4&${
-        category.id > 0 ? `category=${category.id}&` : ""
-      }sortBy=${sort.sortProperty}&order=${order}${
-        searchValue ? `&name=${searchValue}` : ""
-      }`
-    )
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        setItems(data);
+    axios
+      .get(
+        `https://628e18f4368687f3e7104a3b.mockapi.io/items?page=${currentPage}&limit=4&${
+          category.id > 0 ? `category=${category.id}&` : ""
+        }sortBy=${sort.sortProperty}&order=${order}${
+          searchValue ? `&name=${searchValue}` : ""
+        }`
+      )
+      .then((res) => {
+        setItems(res.data);
         setIsLoading(false);
       });
     window.scrollTo(0, 0);
   }, [category, sort, order, searchValue, currentPage]);
 
-  const pizzas = items
-    .map((item) => <PizzaBlock key={item.id} {...item} />);
+  const pizzas = items.map((item) => <PizzaBlock key={item.id} {...item} />);
   const skeletons = [...Array(4)].map((_, index) => <Skeleton key={index} />);
 
   return (
     <div className="container">
       <div className="content__top">
-        <Categories
-          value={category}
-          onCahngeCategory={onCahngeCategory}
-        />
+        <Categories value={category} onCahngeCategory={onCahngeCategory} />
         <Sort />
       </div>
       <h2 className="content__title">{category.name}</h2>
