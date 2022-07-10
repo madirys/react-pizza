dbec7b3995f55a40181aa18ae03c4eddbc408c5c
Madi Rysbek <madirysbek@yandex.kz>
2022-07-10 22:00:37 +0600

    ref: pages lazy chunks

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,35 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import { Route, Routes } from 'react-router-dom';
-
 import Home from './pages/Home';
-import Cart from './pages/Cart';
-import NotFound from './pages/NotFound';
-
 import './scss/app.scss';
-import FullProduct from './pages/FullProduct';
 import DefaultLayout from './layouts/DefaultLayout';
+
+
+// use react-loadable package for server side rendering
+const Cart = React.lazy(() => import(/* webpackChunkName: "Cart" */ './pages/Cart'));
+const FullProduct = React.lazy(() => import(/* webpackChunkName: "FullProduct" */ './pages/FullProduct'));
+const NotFound = React.lazy(() => import(/* webpackChunkName: "NotFound" */ './pages/NotFound'));
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<DefaultLayout />}>
         <Route path="" element={<Home />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/product/:id" element={<FullProduct />} />
-        <Route path="*" element={<NotFound />} />
+        <Route path="/cart" element={
+          <Suspense fallback={<div>Загрузка...</div>}>
+            <Cart />
+          </Suspense>
+        } />
+        <Route path="/product/:id" element={
+          <Suspense fallback={<div>Загрузка...</div>}>
+            <FullProduct />
+          </Suspense>
+        } />
+        <Route path="*" element={
+          <Suspense fallback={<div>Загрузка...</div>}>
+            <NotFound />
+          </Suspense>
+        } />
       </Route>
     </Routes>
   );
